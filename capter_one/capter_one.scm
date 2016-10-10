7203c6179e5c9dbf926b953379a89f3d798a2d5d
uruk <lucius0720@hotmail.com>
2016-10-10 21:10:35 +0800

    Change the plan of Learning Scheme

--- capter_one/capter_one.scm
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env guile
-(define (sum l)
-  (if (null? (cdr l)) (car l)
-      (+ (car l) (sum (cdr l)))))
-(define (multi-sum x . y)
-  (if (null? y) x
-      (+ x (apply multi-sum y))))
-

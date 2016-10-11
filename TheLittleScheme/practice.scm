142d3124e3d98d278a3724afa34c276e79021d7d
uruk <lucius0720@hotmail.com>
2016-10-11 23:28:22 +0800

    complete chapter four

--- TheLittleScheme/practice.scm
+++ TheLittleScheme/practice.scm
@@ -57,3 +57,135 @@
         (eq? old2 (car l))) (cons new (cdr l)))
    (else (cons (car l)
                (subset2 new old1 old2 (cdr l))))))
+(define (multirember a l)
+  (cond
+   ((null? l) l)
+   (else (cond
+          ((eq? a (car l)) (multirember a (cdr l)))
+          (else (cons (car l)
+                      (multirember a (cdr l))))))))
+(define (multiinsertR new old l)
+  (cond
+   ((null? l) l)
+   (else (cond
+          ((eq? old (car l)) (cons old
+                                   (cons new
+                                         (multiinsertR new old (cdr l)))))
+          (else (cons (car l)
+                      (multiinsertR new old (cdr l))))))))
+(define (multiinsertL new old l)
+  (cond
+   ((null? l) l)
+   (else (cond
+          ((eq? old (car l)) (cons new
+                                   (cons old (multiinsertL new old (cdr l)))))
+          (else (cons (car l) (multiinsertL new old (cdr l))))))))
+
+(define (multisubset new old l)
+  (cond
+   ((null? l) l)
+   (else (cond
+          ((eq? old (car l)) (cons new (multisubset new old (cdr l))))
+          (else (cons (car l)
+                      (multisubset new old (cdr l))))))))
+
+(define (add1 n)
+  (+ 1 n))
+
+(define (sub1 n)
+  (- n 1))
+
+(define (o+ a b)
+  (cond
+   ((zero? b) a)
+   (else (add1 (o+ a (sub1 b))))))
+
+(define (o- a b)
+  (cond
+   ((zero? b) a)
+   (else (sub1 (o- a (sub1 b))))))
+
+(define (addup l)
+  (cond
+   ((null? l) 0)
+   (else (o+ (car l) (addup (cdr l))))))
+
+(define (o* a b)
+  (cond
+   ((zero? b) 0)
+   (else (o+ a (o* a (sub1 b))))))
+
+(define (tup+ tup1 tup2)
+  (cond
+   ((null? tup2) tup1)
+   ((null? tup1) tup2)
+   (else (cons (o+ (car tup1) (car tup2))
+               (tup+ (cdr tup1) (cdr tup2))))))
+
+(define (o> a b)
+  (cond
+   ((zero? a) #f)
+   ((zero? b) #t)
+   (else (o> (sub1 a) (sub1 b)))))
+
+(define (o< a b)
+  (cond
+   ((zero? b) #f)
+   ((zero? a) #t)
+   (else (o< (sub1 a) (sub1 b)))))
+
+(define (o= a b)
+  (cond
+   ((or (o< a b) (o> a b)) #f)
+   (else #t)))
+
+(define (expt a b)
+  (cond
+   ((zero? b) 1)
+   (else (o* a (expt a (sub1 b))))))
+
+(define (length l)
+  (cond
+   ((null? l) 0)
+   (else (add1 (length (cdr l))))))
+
+(define (pick num l)
+  (cond
+   ((zero? (sub1 num)) (car l))
+   (else (pick (sub1 num) (cdr l)))))
+
+(define (rempick n l)
+  (cond
+   ((one? n) (cdr l))
+   (else (cons (car l) (rempick (sub1 n) (cdr l))))))
+
+(define (no-num l)
+  (cond
+   ((null? l) l)
+   ((number? (car l)) (no-num (cdr l)))
+   (else (cons (car l) (no-num (cdr l))))))
+
+(define (all-num l)
+  (cond
+   ((null? l) l)
+   (else
+    (cond
+     ((number? (car l)) (cons (car l) (all-num (cdr l))))
+     (else (all-num (cdr l)))))))
+
+(define (eqan? a b)
+  (cond
+   ((and (number? a) (number? b)) (= a b))
+   ((or (number? a) (number? b)) #f)
+   (else (eq? a b))))
+
+(define (occur a l)
+  (cond
+   ((null? l) 0)
+   (else (cond
+          ((eqan? a (car l))
+           (add1 (occur a (cdr l))))
+          (else (occur a (cdr l)))))))
+
+(define (one? a)
+  (= a 1))

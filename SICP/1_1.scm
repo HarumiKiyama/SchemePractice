1dd4175678338780d328d73e3112ad8d0093a81f
harumi <h.kiyama0720@gmail.com>
2019-03-21 23:46:48 +0800

    add sicp 1.11

--- SICP/1_1.scm
+++ SICP/1_1.scm
@@ -1,1 +1,17 @@
 ;; SCIP 1_1 Assignments
+;; 1.11
+(define (f n)
+  (if (< n 3)
+      n
+      (+ (f (- n 1))
+         (* 2 (f (- n 2)))
+         (* 3 (f (- n 3)))))
+  )
+
+(define (f_iter a b c count)
+  (if (< count 3)
+      a
+      (f_iter (+ a (* 2 b) (* 3 c))
+              a b (- count 1)
+              ))
+  )
